0ffe094fe5a48d28f0e18b096a2496a01896b99a
gdscPolyibadan <gdsctpichapter@gmail.com>
2022-04-23 03:43:59 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const user = [
+    {
+        firstName: "Qudus",
+        lastName: "Akeem",
+        email: "akeemqudus2016@gmail.com",
+        phone: 07065967020,
+        isStudent: true
+    },
+    {
+        firstName: "Olaitan",
+        lastName: "Temitope",
+        email: "temitol@gmail.com",
+        phone: 07065967020,
+        isStudent: true
+    },
+    {
+        name: "Kikelomo Abraham",
+        email: "kiks.braham@gmail.com",
+        phone: 07065967020,
+        isStudent: false
+    }
+];
+
+
+console.log(user[0].firstName + " "+ user[0].lastName);
+
+console.log(`This user's firstname is ${user[0].firstName}, and the full name is ${user[0].lastName} ${user[0].firstName}`);
+
+
+//() Parenthesis
+
+//[] Square bracket or Array bracket
+
+// {} Curly braces
